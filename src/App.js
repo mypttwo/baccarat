da0ccc241f932bca0a1946535257876dcb56b90f
mypttwo <mypt.two@gmail.com>
2020-02-02 23:45:57 +0530

    Adding support for multiple bootstrap themes  ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
 import Baccarat from './Containers/Baccarat/baccarat';
+import themes from './Utils/themes';
 
 
 class App extends Component {
+  state = {
+    stylePath : themes['pulse'].url
+  }
 
   render(){
     return (
       <React.Fragment>
+        <link rel="stylesheet" type="text/css" href={this.state.stylePath} />
         <div className="App">
           <Baccarat></Baccarat>
         </div>
